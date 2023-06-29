7b197988fc9e9d5dcacbc8356c8af369256b4a6d
atomknack <110350925+atomknack@users.noreply.github.com>
2023-06-29 22:34:28 +0400

    Update Collector.cs

--- Mirror/Assets/3rdPerson/Collector.cs
+++ Mirror/Assets/3rdPerson/Collector.cs
@@ -1,33 +1,48 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UKnack.Attributes;
 using UKnack.Values;
 using UnityEngine;
 
-public class Collector : MonoBehaviour
+public class Collector : NetworkBehaviour
 {
     [SerializeField]
     [ValidReference]
     private SOValueMutable<int> _collectedValue;
 
+    [SyncVar(hook = nameof(OnCliendGetNewCollectedValue))]
+    private int _collected;
+
     private void OnCollisionEnter(Collision hit)
     {
+        if (isServer == false)
+            return;
+
         var other = hit.gameObject;
         if (other == null)
             return;
-
-
 
         var collectibleGold = other.GetComponent<CollectibleGold>();
         //Debug.Log(collectibleGold);
         if (collectibleGold != null && collectibleGold.TryCollect(out int worth, transform))
         {
-            _collectedValue.SetValue(_collectedValue.GetValue() + worth);
+            //_collectedValue.SetValue(_collectedValue.GetValue() + worth);
+            _collected += worth;
         }    
     }
 
-    private void Awake()
+    private void OnCliendGetNewCollectedValue(int oldCollected, int newCollected)
     {
+        _collectedValue.SetValue(newCollected);
+
+    }
+
+    private void OnEnable()
+    {
+        if (isLocalPlayer == false)
+            return;
+
         if (_collectedValue == null)
             throw new System.ArgumentNullException(nameof(_collectedValue));
     }
