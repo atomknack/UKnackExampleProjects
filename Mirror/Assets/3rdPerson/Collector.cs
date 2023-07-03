f266f104ddf047362f1dbe00428c2ee6239a44cf
atomknack <110350925+atomknack@users.noreply.github.com>
2023-07-03 15:54:16 +0400

    updated to use dictionary

--- Mirror/Assets/3rdPerson/Collector.cs
+++ Mirror/Assets/3rdPerson/Collector.cs
@@ -2,6 +2,7 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UKnack.Attributes;
+using UKnack.KeyValues;
 using UKnack.Values;
 using UnityEngine;
 
@@ -11,8 +12,14 @@ public class Collector : NetworkBehaviour
     [ValidReference]
     private SOValueMutable<int> _collectedValue;
 
+    [SerializeField]
+    [ValidReference]
+    private SOKeyValueMutable<int, int> _collectedDictionary_netId_amount;
+
     [SyncVar(hook = nameof(OnCliendGetNewCollectedValue))]
     private int _collected;
+
+    private int _identityNetId;
 
     private void OnCollisionEnter(Collision hit)
     {
@@ -29,23 +36,39 @@ public class Collector : NetworkBehaviour
         {
             //_collectedValue.SetValue(_collectedValue.GetValue() + worth);
             _collected += worth;
-        }  
-        
+
+            _collectedDictionary_netId_amount[_identityNetId] = _collected;
+            Debug.Log($"id {_identityNetId} changed value to {_collectedDictionary_netId_amount[_identityNetId]}");
+        }
+
+
+
         if (_collected > 15)
             NetworkManager.singleton.ServerChangeScene("Level2");
     }
 
     private void OnCliendGetNewCollectedValue(int oldCollected, int newCollected)
     {
+        if (isOwned == false) 
+            return;
+
+        Debug.Log($"Hook called for {_identityNetId}, s: {isServer} {isServerOnly} c: {isClient} {isClientOnly} o: {isOwned} lp: {isLocalPlayer}");
         _collectedValue.SetValue(newCollected);
 
     }
 
-    private void OnEnable()
+    public override void OnStartServer()
     {
-        if (isLocalPlayer == false)
-            return;
+        Debug.Log($"OnStartServer: {isServer} {isServerOnly} {isClient} {isClientOnly} {isOwned} {isLocalPlayer}");
+        if (_collectedDictionary_netId_amount == null)
+            throw new System.ArgumentNullException(nameof(_collectedDictionary_netId_amount));
 
+        _identityNetId = (int)netIdentity.netId;
+    }
+
+    public override void OnStartClient()
+    {
+        Debug.Log($"OnStartClient: {isServer} {isServerOnly} {isClient} {isClientOnly} {isOwned} {isLocalPlayer}");
         if (_collectedValue == null)
             throw new System.ArgumentNullException(nameof(_collectedValue));
     }
