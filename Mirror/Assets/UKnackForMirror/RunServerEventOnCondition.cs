923b9f0753368605866d670e62d9e72fd8241793
atomknack <110350925+atomknack@users.noreply.github.com>
2023-07-08 18:42:10 +0400

    Update RunServerEventOnCondition.cs
    formated document

--- Mirror/Assets/UKnackForMirror/RunServerEventOnCondition.cs
+++ Mirror/Assets/UKnackForMirror/RunServerEventOnCondition.cs
@@ -8,14 +8,15 @@ using UnityEngine.Events;
 
 public sealed class RunServerEventOnCondition : NetworkBehaviour, ISubscriberToEvent<int, int>
 {
-	public enum Condition{
-		Equal,
-		NotEqual,
+    public enum Condition
+    {
+        Equal,
+        NotEqual,
         Smaller,
         EqualOrSmaller,
         Bigger,
-		EqualOrBigger,
-	}
+        EqualOrBigger,
+    }
 
     [SerializeField]
     [ValidReference]
@@ -27,47 +28,47 @@ public sealed class RunServerEventOnCondition : NetworkBehaviour, ISubscriberToE
     [SerializeField]
     private int _thanBaseValue;
 
-	[SerializeField]
-	private UnityEvent<int, int> _then_InvokeEvent;
+    [SerializeField]
+    private UnityEvent<int, int> _then_InvokeEvent;
 
     public string Description => nameof(RunServerEventOnCondition);
 
     public void OnEventNotification(int key, int value)
     {
-		if (CheckCondition(_whenAnyValue, _thanBaseValue, value))
+        if (CheckCondition(_whenAnyValue, _thanBaseValue, value))
             _then_InvokeEvent.Invoke(key, value);
     }
 
     public override void OnStartServer()
-	{
-		if (_givenKeyValue == null)
-			throw new System.ArgumentNullException(nameof(_givenKeyValue));
+    {
+        if (_givenKeyValue == null)
+            throw new System.ArgumentNullException(nameof(_givenKeyValue));
         _givenKeyValue.Subscribe(this);
-	}
+    }
     public override void OnStopServer()
     {
-		Debug.Log($"RunServerEventOnCount OnStopServer called");
+        Debug.Log($"RunServerEventOnCount OnStopServer called");
         _givenKeyValue.UnsubscribeNullSafe(this);
     }
 
-	private static bool CheckCondition(Condition condition, int baseValue, int comparedValue)
-	{
-		switch (condition)
-		{
-			case Condition.Equal:
-				return comparedValue == baseValue;
-			case Condition.NotEqual:
-				return comparedValue != baseValue;
-			case Condition.Smaller:
-				return comparedValue < baseValue;
-			case Condition.EqualOrSmaller:
-				return comparedValue <= baseValue;
-			case Condition.Bigger:
-				return comparedValue > baseValue;
-			case Condition.EqualOrBigger:
-				return comparedValue >= baseValue;
-			default:
-				throw new System.ArgumentException("Unknown condition");
-		}
-	}
+    private static bool CheckCondition(Condition condition, int baseValue, int comparedValue)
+    {
+        switch (condition)
+        {
+            case Condition.Equal:
+                return comparedValue == baseValue;
+            case Condition.NotEqual:
+                return comparedValue != baseValue;
+            case Condition.Smaller:
+                return comparedValue < baseValue;
+            case Condition.EqualOrSmaller:
+                return comparedValue <= baseValue;
+            case Condition.Bigger:
+                return comparedValue > baseValue;
+            case Condition.EqualOrBigger:
+                return comparedValue >= baseValue;
+            default:
+                throw new System.ArgumentException("Unknown condition");
+        }
+    }
 }
