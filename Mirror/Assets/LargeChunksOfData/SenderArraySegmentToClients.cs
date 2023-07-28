b9cdb1d3fa559efae6e6040bcf6fb9e80cbecfe8
atomknack <110350925+atomknack@users.noreply.github.com>
2023-07-28 09:41:02 +0400

    lets look what underlying array size

--- Mirror/Assets/LargeChunksOfData/SenderArraySegmentToClients.cs
+++ Mirror/Assets/LargeChunksOfData/SenderArraySegmentToClients.cs
@@ -74,7 +74,7 @@ public class SenderArraySegmentToClients : NetworkBehaviour
         //Debug.Log($"RpcSendedDataToClient called from {target}");
         //Debug.Log($"RpcSendedDataToClient called from id {target.connectionId}, with {transfer}");
         Debug.Log($"RpcSendedDataToClient {_dataCount} and new transfer is: {transfer.Count}, checker: {currentClientCountShouldBe}");
-
+        Debug.Log($"{transfer[0]} {transfer.Count} {transfer.Array.Length}");
 
         if (currentClientCountShouldBe != _dataCount)
             throw new System.Exception($"client have {_dataCount}, and server expecting it to have {currentClientCountShouldBe}, there is tear somewhere");
